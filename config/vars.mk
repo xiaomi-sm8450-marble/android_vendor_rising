b98359bb27b959ea78a5c9530e0e4b6930abb643
minaripenguin <minaripenguin@users.noreply.github.com>
2024-04-26 12:18:01 +0800

    vars: Load boot image profile from frameworks/base
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/vars.mk
+++ config/vars.mk
@@ -18,7 +18,7 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 # Use a profile based boot image for this device. Note that this is currently a
 # generic profile and not Android Go optimized.
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := art/build/boot/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Disable dex2oat debug
 USE_DEX2OAT_DEBUG := false
