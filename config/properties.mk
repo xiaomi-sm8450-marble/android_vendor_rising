7c0719df83c721ac5798998d3c1b9063350fd0c6
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-17 10:23:07 +0800

    Add properties for mainline spoofing
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/properties.mk
+++ config/properties.mk
@@ -105,3 +105,18 @@ PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.default_launcher=0 \
     persist.sys.quickswitch_pixel_shipped=0
 endif
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_mainline_BRAND?=google \
+    persist.sys.pihooks_mainline_DEVICE?=caiman \
+    persist.sys.pihooks_mainline_MANUFACTURER?=Google \
+    persist.sys.pihooks_mainline_PRODUCT?=caiman \
+    ro.product.model_for_attestation?=Pixel 9 Pro \
+    ro.product.brand_for_attestation?=google \
+    ro.product.name_for_attestation?=caiman \
+    ro.product.device_for_attestation?=caiman \
+    ro.product.manufacturer_for_attestation?=Google
+    
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PIHOOKS_BUILD_FINGERPRINT="google/caiman/caiman:14/AD1A.240530.047.U1/12150698:user/release-keys" \
+    PIHOOKS_MODEL_SPOOF="Pixel 9 Pro"
